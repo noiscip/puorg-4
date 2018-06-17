3551a38d7576c50a9d5d5ed8ac5ae83a95880496
bitKJS <wnstn8029@naver.com>
2018-06-17 19:42:59 +0900

    no message

--- src/main/java/kr/or/picsion/operation/controller/OperationController.java
+++ src/main/java/kr/or/picsion/operation/controller/OperationController.java
@@ -1,14 +1,17 @@
 package kr.or.picsion.operation.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.View;
 
 import kr.or.picsion.operation.service.OperationService;
+import kr.or.picsion.operationapply.dto.OperationApply;
 
 @Controller
-@RequestMapping("")
 public class OperationController {
 
 	@Autowired
@@ -16,4 +19,14 @@ public class OperationController {
 
 	@Autowired
 	private OperationService operationService;
+	
+	
+	
+	@RequestMapping(value="operok.ps")
+	public String OperationApplyok(OperationApply operationApply, HttpSession session) {
+		System.out.println("OperationApplyok컨트롤러");
+		System.out.println(operationApply);
+		return "board.boardInfo";
+	}
+		
 }
