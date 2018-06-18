e88dbb9949574ea2538fff60ae4d03b5add2249c
Heez <heez1734@gmail.com>
2018-06-18 14:53:47 +0900

    no message

--- src/main/java/kr/or/picsion/comment/dao/CommentDao.java
+++ src/main/java/kr/or/picsion/comment/dao/CommentDao.java
@@ -9,5 +9,7 @@ public interface CommentDao {
 	public int insertComment(Comment comment);
 	public int updateComment(Comment comment);
 	public List<Comment> commentList(int brdNo);
+	public List<Comment> picCommentList(int picNo); //사진에 대한 댓글리스트
+	public int picInsertComment(Comment comment);	//사진에 댓글 저장
 	public int deleteComment(int cmtNo);
 }
