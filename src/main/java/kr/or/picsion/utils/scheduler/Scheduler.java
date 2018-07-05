0a4874b73670ba8c4db0390d6cfd92f6c264e190
SexyYoon <sky4238s@gmail.com>
2018-07-05 09:16:15 +0900

    스케쥴러

--- src/main/java/kr/or/picsion/utils/scheduler/Scheduler.java
+++ src/main/java/kr/or/picsion/utils/scheduler/Scheduler.java
@@ -3,7 +3,13 @@
  */
 package kr.or.picsion.utils.scheduler;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import kr.or.picsion.user.dao.UserDao;
+import kr.or.picsion.user.dto.User;
 
 /**
  * @project Final_Picsion
@@ -12,11 +18,26 @@ import org.springframework.scheduling.annotation.Scheduled;
  * @date 2018. 7. 4.
  */
 
+@Service
 public class Scheduler {
-
-	@Scheduled(cron="*/30 * * * * *")
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Scheduled(cron="0 0 0 * * *")
 	public void deleteMsg() {
-		System.out.println("하하하");
+		System.out.println("스케쥴러");
+		User user = new User();
+		user.setUserId("scheduler");
+		user.setPwd("1234");
+		user.setUserName("24:00:00");
+		
+		UserDao userDao = sqlSession.getMapper(UserDao.class);
+		
+		userDao.registerUser(user);
+		userDao.insertProfile(user.getUserNo());
+		userDao.insertAccount(user.getUserNo());
+		
 	}
 	
 }
