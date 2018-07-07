673b986ba116cabdbb3ec90cd7179bb7b748ee11
Heez <heez1734@gmail.com>
2018-07-07 19:14:46 +0900

    메인 화면 수정, 결제 페이지 트랜잭션 추가, 결제 시 판매자 금액 증가, 트랜잭션 3구간 추가

--- src/main/java/kr/or/picsion/utils/VisionApi.java
+++ src/main/java/kr/or/picsion/utils/VisionApi.java
@@ -65,7 +65,7 @@ public class VisionApi {
 		String logocheck=detectLogos(filePath);				//vision : 로고감지
 		String safecheck=detectSafeSearch(filePath);		//vision : 유해감지
 		List<String> labelList=detectLabels(filePath);		//vision : 태그뽑기
-//		List<Face> faceList = detectFaces(uploadedPath);	//vision : 얼굴감지
+ 		List<Face> faceList = detectFaces(filePath);	//vision : 얼굴감지
 		for(String label : detectWebDetections(filePath)) {
 			labelList.add(label);
 		}
@@ -77,7 +77,7 @@ public class VisionApi {
 		model.addAttribute("label", labelList);
 		model.addAttribute("label2", labelListKo);
 		model.addAttribute("picPath",picturePath);
-//		model.addAttribute("face",faceList);
+ 		model.addAttribute("face",faceList);
 		
 	}	
 	
